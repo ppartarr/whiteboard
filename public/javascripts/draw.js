514da68fbc6b4ab0a6f6341186c2af4cd08a04c2
ppartarr <ppartarr@outlook.com>
2016-11-11 18:06:25 +0000

    drawing is now possible

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,79 +1,79 @@
-// LOOK, PRETTY CIRCLES
-// Max size of circle when user goes fast
-tool.maxDistance = 50;
-// Returns an object specifying a semi-random color
-function randomColor() {
-  return {
-    red: 0,
-    green: Math.random(),
-    blue: Math.random(),
-    alpha: ( Math.random() * 0.25 ) + 0.05
-  };
-}
-
-function onMouseDrag(event) {
-  var x = event.middlePoint.x;
-  var y = event.middlePoint.y;
-  var radius = event.delta.length / 2;
-  var color = randomColor();
-  drawCircle( x, y, radius, color );
-  emitCircle( x, y, radius, color );
-}
-
-
-function drawCircle( x, y, radius, color ) {
-  var circle = new Path.Circle( new Point( x, y ), radius );
-  circle.fillColor = new RgbColor( color.red, color.green, color.blue, color.alpha );
-  view.draw();
-}
-
-function emitCircle( x, y, radius, color ) {
-  var sessionId = io.socket.sessionid;
-  var data = {
-    x: x,
-    y: y,
-    radius: radius,
-    color: color
-  };
-  io.emit( 'drawCircle', data, sessionId )
-  //console.log( data )
-}
-
-io.on( 'drawCircle', function( data ) {
-  console.log( 'drawCircle event recieved:', data );
-  drawCircle( data.x, data.y, data.radius, data.color );
-})
-
-// DRAW A LINE
-
-//var myPath;
-//
-//function onMouseDown(event) {
-//	drawLine(myPath);
-//	emitLine(myPath);
+//// LOOK, PRETTY CIRCLES!
+//// Max size of circle when user goes fast
+//tool.maxDistance = 50;
+//// Returns an object specifying a semi-random color
+//function randomColor() {
+//  return {
+//    red: 0,
+//    green: Math.random(),
+//    blue: Math.random(),
+//    alpha: ( Math.random() * 0.25 ) + 0.05
+//  };
 //}
 //
 //function onMouseDrag(event) {
-//	myPath.add(event.point);
+//  var x = event.middlePoint.x;
+//  var y = event.middlePoint.y;
+//  var radius = event.delta.length / 2;
+//  var color = randomColor();
+//  drawCircle( x, y, radius, color );
+//  emitCircle( x, y, radius, color );
 //}
 //
-//function drawLine(myPath) {
-//  myPath = new Path();
-//  myPath.strokeColor = 'black';
+//
+//function drawCircle( x, y, radius, color ) {
+//  var circle = new Path.Circle( new Point( x, y ), radius );
+//  circle.fillColor = new RgbColor( color.red, color.green, color.blue, color.alpha );
 //  view.draw();
 //}
 //
-//function emitLine(myPath){
+//function emitCircle( x, y, radius, color ) {
 //  var sessionId = io.socket.sessionid;
 //  var data = {
-//    myPath;
+//    x: x,
+//    y: y,
+//    radius: radius,
+//    color: color
 //  };
-//  io.emit('drawLine', data, sessionId)
-//  console.log(data);
+//  io.emit( 'drawCircle', data, sessionId )
+//  console.log( data )
 //}
 //
-//io.on( 'drawLine', function( data ) {
-//  console
-//  drawCircle(myPath);
+//io.on( 'drawCircle', function( data ) {
+//  drawCircle( data.x, data.y, data.radius, data.color );
 //})
+
+// DRAW A LINE
+var myPath;
+
+function onMouseDown(event) {
+	myPath = new Path();
+	myPath.strokeColor = 'black';
+}
+
+function onMouseDrag(event) {
+	myPath.add(event.point);
+	view.draw();
+	drawLine(myPath);
+	emitLine(myPath);
+}
+
+
+function drawLine(myPath) {
+  this.myPath.add(event.point);
+  view.draw();
+}
+
+function emitLine(myPath){
+  var sessionId = io.socket.sessionid;
+  var data = {
+    myPath: myPath
+  };
+  io.emit('onMouseDrag', data, sessionId)
+  console.log(data);
+}
+
+io.on( 'onMouseDrag', function( data ) {
+  drawLine(myPath);
+})
 
